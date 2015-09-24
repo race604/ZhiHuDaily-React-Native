007dd90f9fc57388200b356f22390c39ea7a3da4
race604 <race604@gmail.com>
2015-09-24 23:58:01 +0800

    Add standalone APK support

--- android/app/src/main/java/com/race604/zhihu/daily/MainActivity.java
+++ android/app/src/main/java/com/race604/zhihu/daily/MainActivity.java
@@ -11,14 +11,50 @@ import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.race604.rn.component.MyReactPackage;
 import com.rctzhihudaily.BuildConfig;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
 
+    private static final String JSBUNDLE_FILE = "ReactNativeDevBundle.js";
     private ReactInstanceManager mReactInstanceManager;
     private ReactRootView mReactRootView;
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+
+    private void prepareJSBundle() {
+        File targetFile = new File(getFilesDir(), JSBUNDLE_FILE);
+        if (!targetFile.exists()) {
+            try {
+                OutputStream out = new FileOutputStream(targetFile);
+                InputStream in = getAssets().open(JSBUNDLE_FILE);
+
+                copyFile(in, out);
+                out.close();
+                in.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prepareJSBundle();
+
         mReactRootView = new ReactRootView(this);
 
         mReactInstanceManager = ReactInstanceManager.builder()
